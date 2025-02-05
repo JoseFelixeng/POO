4f7f37523d759a73c81065bccf20a3ffc161f259
Jose <jfelixnunes2k15@gmail.com>
2025-02-05 16:19:59 -0300

    att

--- Refazendo/3.py
+++ Refazendo/3.py
@@ -1,10 +1,26 @@
 # Implemente um programa que leia do usuário um total N de números reais e imprima na tela qual foi o maior número digitado.
 
-n = int(input('Quantos numeros serão digitados'))
+n = int(input('Quantos numeros serão digitados '))
 
 
 def test_numeros(n: int) -> int:  
-    cont = maior = menor = 0 
+    cont = maior = menor = 0
 
-    for i in range(0, len(n)):
-        
+    for i in range(0, n):
+        cont = int(input('digite um número \n'))
+
+        if cont > maior:
+            menor = maior
+            maior = cont
+
+    return maior
+
+
+test = test_numeros(n)
+
+print(f'O maior numero é {test}')
+
+
+
+
+
