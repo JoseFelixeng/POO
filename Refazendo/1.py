cc1eb2d31e326fae2aef6cc2d9d9b318b8e0bc05
Jose <jfelixnunes2k15@gmail.com>
2025-02-06 11:44:47 -0300

    att

--- Refazendo/1.py
+++ Refazendo/1.py
@@ -4,27 +4,44 @@
 
 # Exercício 0 - Solução
 
-a = int(input('Informe o 1o. nr.: '))
-b = int(input('Informe o 2o. nr.: '))
-c = int(input('Informe o 3o. nr.: '))
-# Dica: ao invés de ler a entrada do usuário
-# com o comando 'input', vc pode atribuir
-# os valores das entradas diretamente no código
-# quando estiver testando o seu código.
-# Exemplo:
-# a = 5
-# b = 2
-# c = 3
 
-pares = 0
+class pares:
+    '''Defini um par apartir de 3 valores'''
 
-def contar_pares(a: int, b: int, c: int) -> int:
-    soma = 0
-    for num in (a,b,c): 
-        if num%2 == 0: 
-            soma = soma +1
-    return soma 
-pares = contar_pares(a,b,c)
+    def __init__(self, a, b, c):
+        '''Inicialização da classe'''
+        self.a = a
+        self.b = b
+        self.c = c
+        self.soma = 0
 
-print('{} numero(s) par(es)'.format(pares))
+    def contar_pares(self: int, a: int, b: int, c: int) -> int:
+        soma = 0
+        for num in (self.a, self.b, self.c):
+            if num % 2 == 0:
+                self.soma = self.soma + 1
 
+        return self.soma
+
+    def __str__(self):
+        '''Saída da função pares '''
+        return 'Quantos numeros são pares = {}'.format(self.soma)
+
+
+if __name__ == "__main__":
+    a = int(input('Informe o 1o. nr.: '))
+    b = int(input('Informe o 2o. nr.: '))
+    c = int(input('Informe o 3o. nr.: '))
+    # Dica: ao invés de ler a entrada do usuário
+    # com o comando 'input', vc pode atribuir
+    # os valores das entradas diretamente no código
+    # quando estiver testando o seu código.
+    # Exemplo:
+    # a = 5
+    # b = 2
+    # c = 3
+
+    p = pares(a, b, c)
+    p.contar_pares(a, b, c)
+
+    print(p)
--- Refazendo/2.py
+++ Refazendo/2.py
@@ -3,30 +3,46 @@
 # O programa deve informar se eles formam um triângulo e em caso positivo, qual é o triângulo formado 
 # (equilátero, isósceles ou escaleno).
 
-a = int(input('Digite o valor de a: '))
-b = int(input('Digite o valor de b: '))
-c = int(input('Digite o valor de c: '))
+class Triangulo:
+    '''Essa é a classe que defini um triangulo'''
 
-def test_triangulo(a: int, b: int, c: int) -> int:
-    tt = True
-    tpt = ''
-    if a > b + c or b > a + c or c > a + b:
-        tt = False
-        return tt, tpt 
-    elif (a != b) and (a != c) and (b != c):
-        tpt = 'escaleno'
-        return tt, tpt
-    elif (a == b and a != c) or ((a == c and a != b)) or (b == c and b != a ):
-        tpt = 'isoceles'
-        return tt, tpt
-    elif a == b == c:
-        tpt = 'equilatero'
-        return tt, tpt
-    
-resultado = test_triangulo(a, b, c)
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.tt = True
+        self.tpt = ''
 
 
-if resultado[0] == True:
-    print(f'é um triangulo de tipo {resultado[1]}')
-else:
-    print('Não é um triangulo')
+    def test_tipo(self:int , a: int, b: int, c: int) -> int:
+
+        if a > b + c or b > a + c or c > a + b:
+            self.tt = False
+        elif (a != b) and (a != c) and (b != c):
+            self.tpt = 'escaleno'
+        elif (a == b and a != c) or ((a == c and a != b)) or (b == c and b != a ):
+            self.tpt = 'isoceles'
+        elif a == b == c:
+            self.tpt = 'equilatero'
+
+    def test_triangulo(self):
+        if self.tt != True:
+            print('Não é um triangulo')
+        else:
+            print('É Um triangulo')
+
+
+    def __str__(self):
+        return '{}, {}'.format(self.tpt, self.tt)
+
+if __name__ == "__main__":
+    a = int(input('Digite o valor de a: '))
+    b = int(input('Digite o valor de b: '))
+    c = int(input('Digite o valor de c: '))
+
+    t = Triangulo(a, b, c)
+    t.test_tipo(a,b,c)
+    print(t)
+    t.test_triangulo()
+
+
