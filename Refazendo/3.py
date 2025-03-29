9eb1c83453d9c0f40c6662d93b4d0df9347751d9
JoseFelix <jfelixnunes2k15@gmail.com>
2025-03-29 11:20:36 -0300

    Refatorando código com erro de lógica

--- Refazendo/3.py
+++ Refazendo/3.py
@@ -2,25 +2,32 @@
 # maior número digitado.
 
 class Numero:
+    """A Classe deve testar uma entrada de N Números
+
+    Returns:
+        _Inteiro_: _O maior e o menor numero da sequência_
+    """
     '''Essa classe é feita para definir a quantidade de numeros a serem testados'''
 
-    def __init__(self, n):
+    def __init__(self, n: int):
         '''Inicialização da classe'''
         self.n = n
-        self.cont=0
-        self.maior=0
-        self.menor = 0
+        self.maior = None
+        self.menor = None
 
     def test_maior(self):
 
-        for i in range(0, self.n):
-            self.cont = int(input('digite um número \n'))
+        for i in range(self.n):
+            self.atual= int(input(f'Digite um numero n°{i+1}\n'))
 
-            if self.cont > self.maior:
-                self.menor = self.maior
-                self.maior = self.cont
-
-        return self.maior
+            if self.maior is None or self.atual > self.maior:
+                self.maior = self.atual
+                
+            if self.menor is None or self.atual < self.menor:
+                self.menor = self.atual
+            
+ 
+        return self.maior, self.menor 
 
     def __str__(self):
         return 'O maior numero é {} e o menor é {}'.format(self.maior, self.menor)
@@ -29,6 +36,10 @@ class Numero:
 
 if __name__ == "__main__":
     n = int(input('Quantos numeros serão digitados '))
-    r = Numero(n)
-    r.test_maior()
-    print(r)
+    
+    if n <= 0:
+        print("O número digitado é menor que zero, Por favor digite um novo número!!!!")
+    else:
+        r = Numero(n)
+        r.test_maior()
+        print(r)
